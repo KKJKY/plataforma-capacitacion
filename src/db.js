19815c96aeb16db4ced9a367de30e612aa8ff4e6
Bryan Naupari <bryannaupary@gmail.com>
2025-12-16 02:53:36 -0500

    Fix MySQL connection using MYSQL_URL correctly

--- src/db.js
+++ src/db.js
@@ -1,8 +1,11 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 dotenv.config();
 
-const db = await mysql.createConnection(process.env.MYSQL_URL);
-console.log("Conectado a MySQL en correctamente");
+const db = await mysql.createConnection({
+  uri: process.env.MYSQL_URL
+});
 
+console.log("Conectado a MySQL correctamente");
 export default db;
+
