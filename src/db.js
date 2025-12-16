e6d0fc8f5dff535e449b34286a8639a0115eee15
Bryan Naupari <bryannaupary@gmail.com>
2025-12-16 01:29:01 -0500

    Fix MySQL connection for Railway

--- src/db.js
+++ src/db.js
@@ -2,14 +2,14 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connection = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+const db = await mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT),
 });
 
 console.log("Conectado a MySQL en correctamente");
 
-export default connection;
+export default db;
